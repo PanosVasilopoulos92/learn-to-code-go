1f251896e99453e9a9275dd5e60c3a735f8fae1c
PanosV <panosVasilopoulos92@gmail.com>
2023-12-16 15:37:32 +0200

    Adds '048-versions-examples' folder to project.

---
+++ 045-mod-code-depend-01/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	dependency1 "github.com/PanosVasilopoulos92/learn-to-code-go-dependency-1"
+)
+
+func main() {
+	ex1 := dependency1.SayWelcome()
+	ex2 := dependency1.SayGoodBye()
+	fmt.Println(ex1, ex2)
+	fmt.Println()
+	fmt.Println(dependency1.SayWelcome())
+	fmt.Println(dependency1.SayGoodBye())
+
+	s3 := dependency1.Leaving()
+	fmt.Println(s3)
+}
